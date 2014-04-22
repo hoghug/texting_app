362387006883da5b99bbf328d0a7e152509d588a
Peter Tinti & Chris Lind <student@epicodus.com>
2014-04-22 10:43:13 -0700

    user must verify phone number when adding contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,23 @@
 class ContactsController < ApplicationController
   def create
-    @user = User.find(current_user.id)
-    @contact = @user.contacts.new(contact_params)
+    @message = Message.new
+    if contact_params[:conf] == User.find(contact_params[:entry_id]).phone
+      @user = User.find(current_user.id)
+      @contact = @user.contacts.new(entry_id: contact_params[:entry_id])
 
-    if @contact.save
-      redirect_to :back
+      if @contact.save
+        redirect_to :back
+      else
+        redirect_to :back
+      end
     else
+      flash[:alert] = "Phone number does not match."
       redirect_to :back
     end
   end
 
   private
   def contact_params
-    params.require(:contact).permit(:entry_id)
+    params.require(:contact).permit(:entry_id, :conf)
   end
 end
